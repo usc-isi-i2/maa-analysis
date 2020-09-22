0d47e0a598e0e2c6c3146eaf9a825afedc0294bf
saggu <amandeep.s.saggu@gmail.com>
2020-09-21 18:02:21 -0700

    add config

---
+++ analysis/app_config.py
@@ -0,0 +1,3 @@
+# TEXT_EMBEDDING_LARGE_ALL = '/Users/amandeep/Github/maa-analysis/MAA_Datasets/v3.2.0/embedding_files/text_embeddings_all_large.tsv.gz'
+TEXT_EMBEDDING_LARGE_ALL = '/Users/amandeep/Github/maa-analysis/MAA_Datasets/v3.2.0/embedding_files/2000.tsv'
+WIKI_LABELS = '/Users/amandeep/Github/maa-analysis/MAA_Datasets/v3.2.0/qnodes-properties-labels-for-V3.2.0_KB.tsv'
